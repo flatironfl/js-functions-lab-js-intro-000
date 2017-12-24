3532332d183aba6541fd8886caace3c28daff6b0
First Last <flatironcamp.tc99@mamber.net>
2017-12-24 06:02:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ function happyHolidays() {
   return "Happy holidays!";
 }
 
-happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+}
+
+function HappyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`;
+}
+
+function holidayCountdown(holiday, numdays) {
+  return `It's ${numdays} until ${holiday}`;
 }
